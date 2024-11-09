a21cb9831e5682bb0a1f603ee396f01bf2a34d5d
Rabbi Shakir <rabbishakir@xyz.com>
2024-11-09 12:29:40 -0500

    Initial Commit

---
+++ s2Framework/src/s2Framework/TerstNG2ndFile.java
@@ -0,0 +1,11 @@
+package s2Framework;
+
+public class TerstNG2ndFile {
+
+	public static void main(String[] args) {
+		System.out.println("I am the seconf file");
+		System.out.println("Made some changes");
+
+	}
+
+}
---
+++ s2Framework/src/s2Framework/testNGMain.java
@@ -0,0 +1,11 @@
+package s2Framework;
+
+public class testNGMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi I am a framework");
+
+	}
+
+}
